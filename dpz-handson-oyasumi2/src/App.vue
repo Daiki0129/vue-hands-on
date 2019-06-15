9fa7469d95a2486da68e90eb3c257d028348f6b0
Daiki Kunii <dai0106ki@gmail.com>
2019-06-15 19:50:04 +0900

    [add] hands-on

---
+++ dpz-handson-oyasumi2/src/App.vue
@@ -0,0 +1,52 @@
+<template>
+  <div id="app">
+    <p>step{{step}}</p>
+    <OyasumiHeader/>
+    <InputQuestion v-if="step == 0" key="start" @start="handleStart"/>
+    <OyasumiResult :questions="questions" :step="step"/>
+  </div>
+</template>
+
+<script>
+import OyasumiHeader from "./components/OyasumiHeader.vue";
+import InputQuestion from "./components/InputQuestion.vue";
+import OyasumiResult from "./components/OyasumiResult.vue";
+
+export default {
+  name: "app",
+  components: {
+    OyasumiHeader,
+    InputQuestion,
+    OyasumiResult
+  },
+  data() {
+    return {
+      step: 0,
+      questions: {
+        name: "",
+        title: 1
+      }
+    };
+  },
+  methods: {
+    handleStart(startData) {
+      console.log("start");
+      console.log(startData);
+      this.questions.name = startData.name;
+      this.questions.title = startData.title;
+      this.step++;
+    }
+  }
+};
+</script>
+
+<style>
+#app {
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  /* text-align: center; */
+  color: #2c3e50;
+  /* margin-top: 60px; */
+}
+</style>
